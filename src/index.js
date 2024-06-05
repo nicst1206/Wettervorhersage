8bdd6c07da093ed2c17898d722dc9bab7b5aafdf
nicst1206 <nicole@rust-frankfurt.de>
2024-06-05 17:27:32 +0200

    Added Api Integration to City and Temperature

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-function searchForCity(event) {
+function updateWeatherData(response) {
+  console.log(response);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let temperatureElement = document.querySelector("#current-temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "obbt00a19b0447e3fbfabf89040f3c88";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeatherData);
+}
+
+function initiateSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let heading = document.querySelector("#city");
-  heading.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", searchForCity);
+searchFormElement.addEventListener("submit", initiateSearch);
+
+searchCity("Berlin");
