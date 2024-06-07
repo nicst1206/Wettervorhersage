ae068bf18ab281e7821b278ec192ad5e14e38420
nicst1206 <nicole@rust-frankfurt.de>
2024-06-07 14:12:39 +0200

    Inject forecast HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -58,3 +58,30 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", initiateSearch);
 
 searchCity("Berlin");
+
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">
+      <img />
+    </div>
+    <div class="weather-forecast-temperatures">
+      <span class="weather-forecast-temperature-maximum">18°</span>
+      <span class="weather-forecast-temperature-minimum"> 12°</span>
+    </div>
+  </div>
+  `;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
+
+let forecastElement = document.querySelector("#forecast");
+displayForecast();
